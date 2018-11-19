4a84381b89cc02f8517e7554a949401653209634
Erind Hoxha <932400893@cit3-501m-12.acg.ac.nz>
2018-11-19 21:47:36 +1300

    Css

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 
-
+// $('body').css('overflow', 'hidden');
+$('.btn-accomodation').on('click', function (){
+    $('body').css('overflow','auto');
+    $('html,body').animate({
+    scrollTop: $(".container-result").offset().top},
+    'slow');
+});
 
 
 
