fb28166d239e5c02261becd305eb632dd0731cd2
Erind Hoxha <932400893@cit3-501m-12.acg.ac.nz>
2018-11-19 20:04:51 +1300

    start

--- js/chat.js
+++ js/chat.js
@@ -1,8 +1,14 @@
 //SLIDER
-
+$(".card-body").scrollTop();
 $(".chat_icon").on('click', function() {
     $(".chat").slideToggle();
     $("body").toggleClass("overhidden");
+    $(".chat").addClass("toggled");
+})
+
+$("#container-transport").on('click', function() {
+  $(".chat.toggled").slideToggle();
+  $(".chat.toggled").removeClass("toggled");
 })
 
   var hours = new Date().getHours();
@@ -16,32 +22,52 @@ $(".chat_icon").on('click', function() {
   hours=hours%12;
   mid='pm';
   }
-  alert (hours + ':' + minutes + mid);
 
-$(".msg_time").text();
+$(".msg_time").text(hours + ':' + minutes + mid);
 
 $(".first_card").hide();
 $(".user_card").hide();
 
 $(".button-send").on('click', function() {
+  $(".msg_time-sent").text(hours + ':' + minutes + mid);
 
-
-
-    var inputValue = $(".type_msg").val();
+    // SECOND ANSWER
+    var inputValue = $(".type_msg").val().toLowerCase();
     if ($(".button-send").hasClass("sentFirstMessage")) {
-    $(".secondAnswer").text(inputValue);
-    }
-    $(".user_card").css('display', 'flex');
-    $(".user-answer").text(inputValue);
+    $(".user_card_2").css('display', 'flex');
+    $(".secondAnswer").text(inputValue.charAt(0).toUpperCase() + inputValue.slice(1));
+    $(".secondAnswer").addClass("secondAnswerAnswered");
+    $(".secondAnswer").removeClass("secondAnswer");
+    if (("transport").indexOf(inputValue) > -1) {
+      // ADD TRANSPORT STUFF HERE
+      $(".first_card_2").css('display', 'flex');
+      $(".thirdAnswer").text("Sweet, so you need Transportation... Could you let me know, for how many people?")
+      $(".card-body").scrollTop(300);
 
+    } else if (("accommodation").indexOf(inputValue) > -1 || ("accomodation").indexOf(inputValue) > -1 || ("acomodation").indexOf(inputValue) > -1 ) {
+      $(".first_card_2").css('display', 'flex');
+      $(".thirdAnswer").text("Sweet, so you need Accommodation... Could you let me know, for how many people?")
+      $(".card-body").scrollTop(300);
+      // Add accommodation stuff here
+    } else {
+      $(".first_card_2").css('display', 'flex');
+      $(".thirdAnswer").text("Sorry, we don't have any answer for that. Please, fill the input on the website, or alternatively contact us.")
+      $(".card-body").scrollTop(300);
+    }
+    }
+    // SECOND ANSWER
+
+    // FIRST ANSWER
+    $(".user_card").css('display', 'flex');
+    $(".user-answer").text(inputValue.charAt(0).toUpperCase() + inputValue.slice(1));
     $(".first_card").css('display', 'flex');
-    $(".answer").text("Hi " + inputValue + ", are you interested for Accomodation or Transport?");
+    $(".answer").text("Hi " + inputValue.charAt(0).toUpperCase() + inputValue.slice(1) + ", are you interested for Accommodation or Transport?");
     $(".type_msg").val("");
     $(".button-send").addClass('sentFirstMessage');
-
     $(".answer").removeClass('answer');
     $(".user-answer").addClass('firstAnswer');
     $(".user-answer").removeClass('user-answer');
+    // FIRST ANSWER
 })
 
 
