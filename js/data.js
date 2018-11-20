bd4c5a897f084459c1b7f201446d916442954289
Erind Hoxha <erind_cbh@hotmail.com>
2018-11-21 00:20:47 +1300

    idk

--- js/data.js
+++ js/data.js
@@ -1,66 +1,63 @@
 var transport = {
-    types: [
-      {
-        name: "Meet the city that never sleeps.",
-        class: "motorbike",
-        image: "hammer.jpg",
-        description: "Wan Chai and Causeway Bay | 1.8 km from downtown | Near the subway",
-        checked: ["checked", "checked"],
-        unchecked: ["unchecked", "unchecked", "unchecked"],
-        available: "Available",
-        price: "288"
-      },
-      {
-        name: "Hammer",
-        class: "motorbike",
-        image: "lammer.jpeg",
-        description: "Wan Chai and Causeway Bay | 1.8 km from downtown | Near the subway!",
-        checked: ["checked", "checked"],
-        unchecked: ["unchecked", "unchecked", "unchecked"],
-        available: "Available",
-        price: "288"
-      },
-            {
-              name: "Hammer",
-              class: "motorbike",
-              image: "lammer.jpeg",
-              description: "TWan Chai and Causeway Bay | 1.8 km from downtown | Near the subway",
-              checked: ["checked", "checked"],
-              unchecked: ["unchecked", "unchecked", "unchecked"],
-              available: "Available",
-              price: "288"
-            },
-      {
-        name: "Hammer",
-        class: "motorbike",
-        image: "tammer.jpeg",
-        description: "Travel one of the world's most diverse and intriguing cities. City Info Exports will help you experience it all!",
-        checked: ["checked", "checked"],
-        unchecked: ["unchecked", "unchecked", "unchecked"],
-        available: "Available",
-        price: "288"
-      },
-      {
-        name: "Hammer",
-        class: "motorbike",
-        image: "lammer.jpeg",
-        description: "Hammer motorbike, 2007.",
-        checked: ["checked", "checked"],
-        unchecked: ["unchecked", "unchecked", "unchecked"],
-        available: "Available",
-        price: "288"
-      },
-      {
-        name: "Hammer",
-        class: "motorbike",
-        image: "hammer.jpg",
-        description: "Hammer motorbike, 2007.",
-        checked: ["checked", "checked"],
-        unchecked: ["unchecked", "unchecked", "unchecked"],
-        available: "Available",
-        price: "288"
-      },
-    ]
-  };
-
-
+  types: [{
+      name: "Meet the city that never sleeps.",
+      class: "motorbike",
+      image: "hammer.jpg",
+      description: "Wan Chai and Causeway Bay | 1.8 km from downtown | Near the subway",
+      checked: ["checked", "checked"],
+      unchecked: ["unchecked", "unchecked", "unchecked"],
+      available: "Available",
+      price: "288"
+    },
+    {
+      name: "Hammer",
+      class: "motorbike",
+      image: "lammer.jpeg",
+      description: "Wan Chai and Causeway Bay | 1.8 km from downtown | Near the subway!",
+      checked: ["checked", "checked"],
+      unchecked: ["unchecked", "unchecked", "unchecked"],
+      available: "Available",
+      price: "288"
+    },
+    {
+      name: "Hammer",
+      class: "motorbike",
+      image: "lammer.jpeg",
+      description: "TWan Chai and Causeway Bay | 1.8 km from downtown | Near the subway",
+      checked: ["checked", "checked"],
+      unchecked: ["unchecked", "unchecked", "unchecked"],
+      available: "Available",
+      price: "288"
+    },
+    {
+      name: "Hammer",
+      class: "motorbike",
+      image: "tammer.jpeg",
+      description: "Travel one of the world's most diverse and intriguing cities. City Info Exports will help you experience it all!",
+      checked: ["checked", "checked"],
+      unchecked: ["unchecked", "unchecked", "unchecked"],
+      available: "Available",
+      price: "288"
+    },
+    {
+      name: "Hammer",
+      class: "motorbike",
+      image: "lammer.jpeg",
+      description: "Hammer motorbike, 2007.",
+      checked: ["checked", "checked"],
+      unchecked: ["unchecked", "unchecked", "unchecked"],
+      available: "Available",
+      price: "288"
+    },
+    {
+      name: "Hammer",
+      class: "motorbike",
+      image: "hammer.jpg",
+      description: "Hammer motorbike, 2007.",
+      checked: ["checked", "checked"],
+      unchecked: ["unchecked", "unchecked", "unchecked"],
+      available: "Available",
+      price: "288"
+    },
+  ]
+};
--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-
 // $('body').css('overflow', 'hidden');
-$('.btn-accomodation').on('click', function (){
-    $('body').css('overflow','auto');
+$('.btn-accomodation').on('click', function () {
+    $('body').css('overflow', 'auto');
     $('html,body').animate({
-    scrollTop: $(".container-result").offset().top},
-    'slow');
+            scrollTop: $(".container-result").offset().top
+        },
+        'slow');
 });
 
 
@@ -22,53 +22,54 @@ $(".hover").mouseleave(
 Reference: http://jsfiddle.net/BB3JK/47/
 */
 
-$('select').each(function(){
-    var $this = $(this), numberOfOptions = $(this).children('option').length;
-  
-    $this.addClass('select-hidden'); 
+$('select').each(function () {
+    var $this = $(this),
+        numberOfOptions = $(this).children('option').length;
+
+    $this.addClass('select-hidden');
     $this.wrap('<div class="select"></div>');
     $this.after('<div class="select-styled"></div>');
 
     var $styledSelect = $this.next('div.select-styled');
     $styledSelect.text($this.children('option').eq(0).text());
-  
+
     var $list = $('<ul />', {
         'class': 'select-options'
     }).insertAfter($styledSelect);
-  
+
     for (var i = 0; i < numberOfOptions; i++) {
         $('<li />', {
             text: $this.children('option').eq(i).text(),
             rel: $this.children('option').eq(i).val()
         }).appendTo($list);
     }
-  
+
     var $listItems = $list.children('li');
-  
-    $styledSelect.click(function(e) {
+
+    $styledSelect.click(function (e) {
         e.stopPropagation();
-        $('div.select-styled.active').not(this).each(function(){
+        $('div.select-styled.active').not(this).each(function () {
             $(this).removeClass('active').next('ul.select-options').hide();
         });
         $(this).toggleClass('active').next('ul.select-options').toggle();
     });
-  
-    $listItems.click(function(e) {
+
+    $listItems.click(function (e) {
         e.stopPropagation();
         $styledSelect.text($(this).text()).removeClass('active');
         $this.val($(this).attr('rel'));
         $list.hide();
         //console.log($this.val());
     });
-  
-    $(document).click(function() {
+
+    $(document).click(function () {
         $styledSelect.removeClass('active');
         $list.hide();
     });
 
 });
 
-$(".btn-book").on('click', function() {
+$(".btn-book").on('click', function () {
     $(".rating-container").empty();
     console.log($(this).closest(".card-wrapper").attr('data-nr'));
     var dataNr = $(this).closest(".card-wrapper").attr('data-nr');
@@ -102,43 +103,3 @@ $(".btn-book").on('click', function() {
 
     });
 }(jQuery));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
