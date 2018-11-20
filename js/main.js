af5137d093decb94877d06df9c38c3934563a4d5
Erind Hoxha <932400893@cit3-501m-12.acg.ac.nz>
2018-11-21 11:53:07 +1300

    Changed the cards

--- js/main.js
+++ js/main.js
@@ -120,3 +120,11 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+$(window).load(function() {
+    $('.post-module').hover(function() {
+      $(this).find('.description').stop().animate({
+        height: "toggle",
+        opacity: "toggle"
+      }, 300);
+    });
+  });
