6e25b230898f5eb3d4b26a1dde2fa559265e4bac
Erind Hoxha <932400893@cit3-501m-12.acg.ac.nz>
2018-11-25 20:16:03 +1300

    Adding and hiding hte cards that are needed

--- js/chat.js
+++ js/chat.js
@@ -1,26 +1,23 @@
 //SLIDER
 $(".card-body").scrollTop();
-$(".chat_icon").on('click', function() {
-    $(".chat-popup").hide();
-    $(".chat").slideToggle();
-    $(".chat").addClass("toggled");
+$(".chat_icon").on('click', function () {
+  $(".chat-popup").hide();
+  $(".chat").slideToggle();
+  $(".chat").addClass("toggled");
 })
 
-
-  var hours = new Date().getHours();
-  var minutes = new Date().getMinutes();
-  if (minutes.toFixed().length == 1) {
-    minutes = "0" + minutes;
-  }
-  var mid='am';
-  if(hours==0){ //At 00 hours we need to show 12 am
-  hours=12;
-  }
-  else if(hours>12)
-  {
-  hours=hours%12;
-  mid='pm';
-  }
+var hours = new Date().getHours();
+var minutes = new Date().getMinutes();
+if (minutes.toFixed().length == 1) {
+  minutes = "0" + minutes;
+}
+var mid = 'am';
+if (hours == 0) { //At 00 hours we need to show 12 am
+  hours = 12;
+} else if (hours > 12) {
+  hours = hours % 12;
+  mid = 'pm';
+}
 
 $(".msg_time").text(hours + ':' + minutes + mid);
 
@@ -28,62 +25,78 @@ $(".first_card").hide();
 $(".user_card").hide();
 
 
-$(".button-send").on('click', function() {
-    var answer;
-    var answer2;
-    fourthAnswer();
-    thirdAnswer();
-    secondAnswer();
-    firstAnswer();
-    console.log(answer);
-    console.log(answer2);
+$(".button-send").on('click', function () {
+  var answer;
+  var answer2;
+  fourthAnswer();
+  thirdAnswer();
+  secondAnswer();
+  firstAnswer();
+  console.log(answer);
+  console.log(answer2);
 })
+
 function firstAnswer() {
 
   $(".msg_time-sent").text(hours + ':' + minutes + mid);
   // SECOND ANSWER
   var inputValue = $(".type_msg").val().toLowerCase();
-    // FIRST ANSWER
-    $(".user_card").css('visibility', 'visible');
-    $(".first_card").css('display', 'flex');
-    $(".user_card").fadeIn(300);
-    $(".user-answer").text(inputValue.charAt(0).toUpperCase() + inputValue.slice(1));
-    $(".first_card").css('visibility', 'visible');
-    $(".first_card").css('display', 'flex');
-    $(".first_card").fadeIn(300);
-    $(".answer").text("Hi " + inputValue.charAt(0).toUpperCase() + inputValue.slice(1) + ", are you interested for Accommodation or Transport?");
-    $(".type_msg").val("");
-    if ($(".button-send").hasClass('classSentFirstMessage')) {
-      // do nothing
-    } else {
-      $(".button-send").addClass('sentFirstMessage');
-    }
-    $(".answer").removeClass('answer');
-    $(".button-send").addClass('classSentFirstMessage');
-    $(".user-answer").addClass('firstAnswer');
-    $(".user-answer").removeClass('user-answer');
-    // FIRST ANSWER
+  // FIRST ANSWER
+  $(".user_card").css('visibility', 'visible');
+  $(".first_card").css('display', 'flex');
+  $(".user_card").fadeIn(300);
+  $(".user-answer").text(inputValue.charAt(0).toUpperCase() + inputValue.slice(1));
+  $(".first_card").css('visibility', 'visible');
+  $(".first_card").css('display', 'flex');
+  $(".first_card").fadeIn(300);
+  $(".answer").text("Hi " + inputValue.charAt(0).toUpperCase() + inputValue.slice(1) + ", are you interested for Accommodation or Transport?");
+  $(".type_msg").val("");
+  if ($(".button-send").hasClass('classSentFirstMessage')) {
+    // do nothing
+  } else {
+    $(".button-send").addClass('sentFirstMessage');
+  }
+  $(".answer").removeClass('answer');
+  $(".button-send").addClass('classSentFirstMessage');
+  $(".user-answer").addClass('firstAnswer');
+  $(".user-answer").removeClass('user-answer');
+  // FIRST ANSWER
 }
 
 function secondAnswer() {
 
-            
+
   $(".msg_time-sent").text(hours + ':' + minutes + mid);
   // SECOND ANSWER
   var inputValue = $(".type_msg").val().toLowerCase();
 
   if ($(".button-send").hasClass("sentFirstMessage")) {
-    $(".button-send").removeClass('sentFirstMessage');    
+    $(".button-send").removeClass('sentFirstMessage');
     $(".user_card_2").css('visibility', 'visible');
     $(".user_card_2").fadeIn(300);
     $(".secondAnswer").text(inputValue.charAt(0).toUpperCase() + inputValue.slice(1));
     if (("transport").indexOf(inputValue) > -1) {
+
+
+      var info = document.getElementById('summative-template').innerHTML;
+      var template = Handlebars.compile(info);
+      var dataTemplate = template(transport);
+      var templateWrite = document.getElementById('card-container').innerHTML += dataTemplate;
+      
       $(".button-send").addClass('sentSecondMessageTransport');
       // ADD TRANSPORT STUFF HERE
       $(".first_card_2").css('display', 'flex');
       $(".thirdAnswer").text("Sweet, so you need Transportation... Could you let me know, for how many people?")
       $(".card-body").scrollTop(1000);
-    } else if (("accommodation").indexOf(inputValue) > -1 || ("accomodation").indexOf(inputValue) > -1 || ("acomodation").indexOf(inputValue) > -1 ) {
+    } else if (("accommodation").indexOf(inputValue) > -1 || ("accomodation").indexOf(inputValue) > -1 || ("acomodation").indexOf(inputValue) > -1) {
+
+
+      var info = document.getElementById('summative-template').innerHTML;
+      var template = Handlebars.compile(info);
+      var dataTemplate = template(accommodation);
+      var templateWrite = document.getElementById('card-container').innerHTML += dataTemplate;
+
+
       $(".button-send").addClass('sentSecondMessageAccommodation');
       $(".first_card_2").css('display', 'flex');
       $(".thirdAnswer").text("Sweet, so you need Accommodation... Could you let me know, for how many people?")
@@ -92,93 +105,91 @@ function secondAnswer() {
     } else {
       $(".first_card_2").css('display', 'flex');
       $(".thirdAnswer").text("Sorry, we don't have any answer for that. Please, fill the input on the website, or alternatively contact us.")
-    // $(".button-send").addClass('sentFirstMessage');    
+      // $(".button-send").addClass('sentFirstMessage');    
       $(".card-body").scrollTop(1000);
     }
-    }
-    // SECOND ANSWER
+  }
+  // SECOND ANSWER
 }
 
 
 function thirdAnswer() {
-        $(".msg_time-sent").text(hours + ':' + minutes + mid);
-        // SECOND ANSWER
-        var inputValue = $(".type_msg").val().toLowerCase();
-        // Third answer
+  $(".msg_time-sent").text(hours + ':' + minutes + mid);
+  // SECOND ANSWER
+  var inputValue = $(".type_msg").val().toLowerCase();
+  // Third answer
 
-        if ($(".button-send").hasClass("sentSecondMessageTransport")) { 
-          $(".button-send").removeClass("sentSecondMessageTransport");
-          $(".user_card_3").css('display', 'flex');
-          $(".thirdUserAnswer").text(inputValue.charAt(0).toUpperCase() + inputValue.slice(1));
-          // $(".thirdUserAnswer").removeClass('thirdUserAnswer');
-          $(".button-send").addClass('sentThirdMessageTransport');
-          // DO STUFF FOR TRANSPORT HERE
-          if (("1 one").includes(inputValue)) {
-            answer = 1;
-            console.log(answer);
-            howManyPeople();
-          } else if (("2 two").includes(inputValue)) {
-            answer = 2;
-            howManyPeople();
-          } else if (("3 three").includes(inputValue)) {
-            answer = 3;
-            howManyPeople();
-          } else if (("4 four").includes(inputValue)) {
-            answer = 4;
-            howManyPeople();
-          } else if (("5 five").includes(inputValue)) {
-            answer = 5;
-            howManyPeople();
-          } else if (("6 six").includes(inputValue)) {
-            answer = 6;
-            howManyPeople();
-          } 
-          else {
-            $(".first_card_3").css('display', 'flex');
-            $(".card-body").scrollTop(1000);
-            $(".fourthAnswer").text("Sorry, we can't understand that number, please type it again");
-          }
+  if ($(".button-send").hasClass("sentSecondMessageTransport")) {
+    $(".button-send").removeClass("sentSecondMessageTransport");
+    $(".user_card_3").css('display', 'flex');
+    $(".thirdUserAnswer").text(inputValue.charAt(0).toUpperCase() + inputValue.slice(1));
+    // $(".thirdUserAnswer").removeClass('thirdUserAnswer');
+    $(".button-send").addClass('sentThirdMessageTransport');
+    // DO STUFF FOR TRANSPORT HERE
+    if (("1 one").includes(inputValue)) {
+      answer = 1;
+      console.log(answer);
+      howManyPeople();
+    } else if (("2 two").includes(inputValue)) {
+      answer = 2;
+      howManyPeople();
+    } else if (("3 three").includes(inputValue)) {
+      answer = 3;
+      howManyPeople();
+    } else if (("4 four").includes(inputValue)) {
+      answer = 4;
+      howManyPeople();
+    } else if (("5 five").includes(inputValue)) {
+      answer = 5;
+      howManyPeople();
+    } else if (("6 six").includes(inputValue)) {
+      answer = 6;
+      howManyPeople();
+    } else {
+      $(".first_card_3").css('display', 'flex');
+      $(".card-body").scrollTop(1000);
+      $(".fourthAnswer").text("Sorry, we can't understand that number, please type it again");
+    }
 
-        } else if ($(".button-send").hasClass("sentSecondMessageAccommodation")) {
-          $(".button-send").removeClass("sentSecondMessageAccommodation");
-          $(".button-send").removeClass('sentFirstMessage');
-          $(".user_card_3").css('display', 'flex');
-          $(".thirdUserAnswer").text(inputValue.charAt(0).toUpperCase() + inputValue.slice(1));
-          $(".thirdUserAnswer").removeClass('thirdUserAnswer');
-          $(".button-send").addClass('sentThirdMessageAccommodation');
-          // DO STUFF FOR ACCOMMODATION HERE
-                if (("1 one").includes(inputValue)) {
-                  var answer = 1;
-                  console.log(answer);
-                  howManyPeople();
-                } else if (("2 two").includes(inputValue)) {
-                  var answer = 2;
-                  console.log(answer);
-                  howManyPeople();
-                } else if (("3 three").includes(inputValue)) {
-                  var answer = 3;
-                  console.log(answer);
-                  howManyPeople();
-                } else if (("4 four").includes(inputValue)) {
-                  var answer = 4;
-                  console.log(answer);
-                  howManyPeople();
-                } else if (("5 five").includes(inputValue)) {
-                  var answer = 5;
-                  console.log(answer);
-                  howManyPeople();
-                } else if (("6 six").includes(inputValue)) {
-                  var answer = 6;
-                  console.log(answer);
-                  howManyPeople();
-                } 
-                else {
-                  $(".first_card_3").css('display', 'flex');
-                  $(".card-body").scrollTop(1000);
-                  $(".fourthAnswer").text("Sorry, we can't understand that number, please input in the website your options.");
-                }
-        }
-  
+  } else if ($(".button-send").hasClass("sentSecondMessageAccommodation")) {
+    $(".button-send").removeClass("sentSecondMessageAccommodation");
+    $(".button-send").removeClass('sentFirstMessage');
+    $(".user_card_3").css('display', 'flex');
+    $(".thirdUserAnswer").text(inputValue.charAt(0).toUpperCase() + inputValue.slice(1));
+    $(".thirdUserAnswer").removeClass('thirdUserAnswer');
+    $(".button-send").addClass('sentThirdMessageAccommodation');
+    // DO STUFF FOR ACCOMMODATION HERE
+    if (("1 one").includes(inputValue)) {
+      var answer = 1;
+      console.log(answer);
+      howManyPeople();
+    } else if (("2 two").includes(inputValue)) {
+      var answer = 2;
+      console.log(answer);
+      howManyPeople();
+    } else if (("3 three").includes(inputValue)) {
+      var answer = 3;
+      console.log(answer);
+      howManyPeople();
+    } else if (("4 four").includes(inputValue)) {
+      var answer = 4;
+      console.log(answer);
+      howManyPeople();
+    } else if (("5 five").includes(inputValue)) {
+      var answer = 5;
+      console.log(answer);
+      howManyPeople();
+    } else if (("6 six").includes(inputValue)) {
+      var answer = 6;
+      console.log(answer);
+      howManyPeople();
+    } else {
+      $(".first_card_3").css('display', 'flex');
+      $(".card-body").scrollTop(1000);
+      $(".fourthAnswer").text("Sorry, we can't understand that number, please input in the website your options.");
+    }
+  }
+
   function howManyPeople() {
     $(".first_card_3").css('display', 'flex');
     var name = $(".firstAnswer").text();
@@ -191,92 +202,95 @@ function thirdAnswer() {
   }
 }
 
-
 function fourthAnswer() {
   $(".msg_time-sent").text(hours + ':' + minutes + mid);
   // SECOND ANSWER
   var inputValue = $(".type_msg").val().toLowerCase();
   // Third answer
 
-  if ($(".button-send").hasClass("sentMessagePeople")) { 
-    $(".user_card_4").css('display','flex');
+  if ($(".button-send").hasClass("sentMessagePeople")) {
+    $(".user_card_4").css('display', 'flex');
     $(".fourthUserAnswer").text(inputValue.charAt(0).toUpperCase() + inputValue.slice(1));
     // $(".user_card_4").css('display', 'flex');
     // $(".thirdUserAnswer").removeClass('thirdUserAnswer');
-  if (("1 one").includes(inputValue)) {
-    var answer2 = 1;
-    console.log(answer2);
-  } else if (("2 two").includes(inputValue)) {
-    var answer2 = 2;
-    console.log(answer2);
-    howManyDays();
-  } else if (("3 three").includes(inputValue)) {
-    var answer2 = 3;
-    console.log(answer2);
-    howManyDays();
-  } else if (("4 four").includes(inputValue)) {
-    var answer2 = 4;
-    console.log(answer2);
-    howManyDays();
-  } else if (("5 five").includes(inputValue)) {
-    var answer2 = 5;
-    console.log(answer2);
-    howManyDays();
-  } else if (("6 six").includes(inputValue)) {
-    var answer2 = 6;
-    console.log(answer2);
-    howManyDays();
-  } else if (("7 seven").includes(inputValue)) {
-    var answer2 = 7;
-    console.log(answer2);
-    howManyDays();
-  } else if (("8 eight").includes(inputValue)) {
-    var answer2 = 8;
-    console.log(answer2);
-    howManyDays();
-  } else if (("9 nine").includes(inputValue)) {
-    var answer2 = 9;
-    console.log(answer2);
-    howManyDays();
-  } else if (("10 ten").includes(inputValue)) {
-    var answer2 = 10;
-    console.log(answer2);
-    howManyDays();
-  } else if (("11 eleven").includes(inputValue)) {
-    var answer2 = 11;
-    console.log(answer2);
-    howManyDays();
-  } else if (("12 twelve").includes(inputValue)) {
-    var answer2 = 12;
-    console.log(answer2);
-    howManyDays();
-  } else if (("13 thirteen").includes(inputValue)) {
-    var answer2 = 13;
-    console.log(answer2);
-    howManyDays();
-  } else if (("14 fourteen").includes(inputValue)) {
-    var answer2 = 14;
-    console.log(answer2);
-    howManyDays();
-  } else if (("15 fifteen").includes(inputValue)) {
-    var answer2 = 15;
-    console.log(answer2);
-    howManyDays();
-  } 
-  function howManyDays() {
-    $(".first_card_4").css('display', 'flex');
-    $(".fifthAnswer").text("Sweet, you've chosen: " + answer2 + " days. By the answer you've given us, we'll send you through the results.");
-    $(".card-body").scrollTop(1000);
-    $(".button-send").removeClass('sentMessagePeople');
-    setTimeout(goToPageWithResults, 3000)
-    } 
+
+    // HARDCODING
+
+    if (("1 one").includes(inputValue)) {
+      var answer2 = 1;
+      console.log(answer2);
+    } else if (("2 two").includes(inputValue)) {
+      var answer2 = 2;
+      console.log(answer2);
+      howManyDays();
+    } else if (("3 three").includes(inputValue)) {
+      var answer2 = 3;
+      console.log(answer2);
+      howManyDays();
+    } else if (("4 four").includes(inputValue)) {
+      var answer2 = 4;
+      console.log(answer2);
+      howManyDays();
+    } else if (("5 five").includes(inputValue)) {
+      var answer2 = 5;
+      console.log(answer2);
+      howManyDays();
+    } else if (("6 six").includes(inputValue)) {
+      var answer2 = 6;
+      console.log(answer2);
+      howManyDays();
+    } else if (("7 seven").includes(inputValue)) {
+      var answer2 = 7;
+      console.log(answer2);
+      howManyDays();
+    } else if (("8 eight").includes(inputValue)) {
+      var answer2 = 8;
+      console.log(answer2);
+      howManyDays();
+    } else if (("9 nine").includes(inputValue)) {
+      var answer2 = 9;
+      console.log(answer2);
+      howManyDays();
+    } else if (("10 ten").includes(inputValue)) {
+      var answer2 = 10;
+      console.log(answer2);
+      howManyDays();
+    } else if (("11 eleven").includes(inputValue)) {
+      var answer2 = 11;
+      console.log(answer2);
+      howManyDays();
+    } else if (("12 twelve").includes(inputValue)) {
+      var answer2 = 12;
+      console.log(answer2);
+      howManyDays();
+    } else if (("13 thirteen").includes(inputValue)) {
+      var answer2 = 13;
+      console.log(answer2);
+      howManyDays();
+    } else if (("14 fourteen").includes(inputValue)) {
+      var answer2 = 14;
+      console.log(answer2);
+      howManyDays();
+    } else if (("15 fifteen").includes(inputValue)) {
+      var answer2 = 15;
+      console.log(answer2);
+      howManyDays();
+    }
+
+    function howManyDays() {
+      $(".first_card_4").css('display', 'flex');
+      $(".fifthAnswer").text("Sweet, you've chosen: " + answer2 + " days. By the answer you've given us, we'll send you through the results.");
+      $(".card-body").scrollTop(1000);
+      $(".button-send").removeClass('sentMessagePeople');
+      setTimeout(goToPageWithResults, 3000)
+    }
   }
 }
 
 // Get the input field
 var input = $("#type_msg");
 // Execute a function when the user releases a key on the keyboard
-$("#type_msg").on("keyup", function(event) {
+$("#type_msg").on("keyup", function (event) {
   // Cancel the default action, if needed
   event.preventDefault();
   // Number 13 is the "Enter" key on the keyboard
@@ -288,20 +302,18 @@ $("#type_msg").on("keyup", function(event) {
 
 
 function filter(typer, typeOfTransport) {
-    for (var i = 0; i < transport.types.length; i++) {
-      if (transport.types[i].class == typeOfTransport) {
+  for (var i = 0; i < transport.types.length; i++) {
+    if (transport.types[i].class == typeOfTransport) {
       console.log(transport.types[i].class)
       $(typer).text("Hey, here is the results: ")
-      }
     }
+  }
 }
 
 function goToPageWithResults() {
   $(".chat").hide(500);
   $('html,body').animate({
-    scrollTop: $(".container-result").offset().top
-},
-'slow');
+      scrollTop: $(".container-result").offset().top
+    },
+    'slow');
 }
-
-
--- js/main.js
+++ js/main.js
@@ -1,67 +1,65 @@
-
-
 $(".alert").hide();
 // $('body').css('overflow', 'hidden');
 $('.btn-accomodation').on('click', function () {
     var transportOrAccommodation = $("#accom-or-transport").val();
     var peopleValue = $("#people-search").val().slice(0, 1);
     peopleValue = parseInt(peopleValue);
-    var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+    var oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
 
-    var firstDate = $( "#datepicker").datepicker( "getDate" )
-    var secondDate = $( "#datepicker2").datepicker( "getDate" )
+    var firstDate = $("#datepicker").datepicker("getDate")
+    var secondDate = $("#datepicker2").datepicker("getDate")
     var numberOfDays = Math.abs((firstDate.getTime() - secondDate.getTime()) / (oneDay));
 
     console.log(transportOrAccommodation)
     console.log(numberOfDays);
     console.log(peopleValue);
 
-       
 
 
-     if (numberOfDays > 15 || firstDate >= secondDate) {
+
+    if (numberOfDays > 15 || firstDate >= secondDate) {
         $(".alert").show(500);
         $(".line").hide(500);
-     } else {
+    } else {
         // $('body').css('overflow', 'auto');
         $(".alert").hide(500);
         $(".line").show(500);
-    $('html,body').animate({
-        scrollTop: $(".container-result").offset().top
-    },
-    'slow');
-     }
-     if (transportOrAccommodation == "Accommodation") {
+        $('html,body').animate({
+                scrollTop: $(".container-result").offset().top
+            },
+            'slow');
+    }
+    if (transportOrAccommodation == "Accommodation") {
 
         var info = document.getElementById('summative-template').innerHTML;
         var template = Handlebars.compile(info);
         var dataTemplate = template(accommodation);
         var templateWrite = document.getElementById('card-container').innerHTML += dataTemplate;
 
-     } else if (transportOrAccommodation == "Transport") {
+    } else if (transportOrAccommodation == "Transport") {
 
         var info = document.getElementById('summative-template').innerHTML;
         var template = Handlebars.compile(info);
         var dataTemplate = template(transport);
         var templateWrite = document.getElementById('card-container').innerHTML += dataTemplate;
 
-     }
+    }
 
-
-   function doFiltering() {
-    $(".card-wrap").hide();
-    $(".card-wrap." + numberOfDays + "d." + peopleValue + "p").show();
-}
+    // FILTER THE CARDS - ADD THE CLASSES WHICH IS APPROPRIATE .daysd + .peoplep + card-wrap
+    function doFiltering() {
+        $(".card-wrap").hide();
+        $(".card-wrap." + numberOfDays + "d." + peopleValue + "p").show();
+    }
     doFiltering();
 
 
-     $(".btn-book").on('click', function () {
-        var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
-        $("#datepicker2").on('change', function() {
-    
-         $(".price-modal").text('Price: $' + transport.types[dataNr].price * numberOfDays);
+    $(".btn-book").on('click', function () {
+        var oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+        $("#datepicker2").on('change', function () {
+
+            $(".price-modal").text('Price: $' + transport.types[dataNr].price * numberOfDays);
         });
-    
+
         $(".modal-body").css('text-align', 'center');
         $(".rating-container").empty();
         console.log($(this).closest(".card-wrapper").attr('data-nr'));
@@ -141,55 +139,55 @@ $('select').each(function () {
 
 
 
-    $(document).ready(function () {
-        // hide .navbar first
-        $(".follow-up-input").hide();
-        // fade in .navbar
-        $(function () {
-            $(window).scroll(function () {
-                if ($(this).scrollTop() > 3000) {
-                    $('.follow-up-input').fadeOut();
-                }
-                // set distance user needs to scroll before we start fadeIn
-               else if ($(this).scrollTop() > 500) {
-                    $('.follow-up-input').fadeIn();   
-                } else {
-                    $('.follow-up-input').fadeOut();
-                }
-            });
+$(document).ready(function () {
+    // hide .navbar first
+    $(".follow-up-input").hide();
+    // fade in .navbar
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 3000) {
+                $('.follow-up-input').fadeOut();
+            }
+            // set distance user needs to scroll before we start fadeIn
+            else if ($(this).scrollTop() > 500) {
+                $('.follow-up-input').fadeIn();
+            } else {
+                $('.follow-up-input').fadeOut();
+            }
         });
-
     });
+
+});
 
 
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    $("#follow-up-input").css('top', '0px');
-    document.getElementById("follow-up-input").style.transition = "0.3s ease";
-  } else {
-    $("#follow-up-input").css('top', '-60px');
-  }
-  prevScrollpos = currentScrollPos;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        $("#follow-up-input").css('top', '0px');
+        document.getElementById("follow-up-input").style.transition = "0.3s ease";
+    } else {
+        $("#follow-up-input").css('top', '-60px');
+    }
+    prevScrollpos = currentScrollPos;
 }
-$(".reload").on('click', function() {
+$(".reload").on('click', function () {
     location.reload();
 })
-  $(".close-btn").on('click', function() {
-      $(".chat-popup").hide(500);
-  })
+$(".close-btn").on('click', function () {
+    $(".chat-popup").hide(500);
+})
 
-  $( function() {
-    $( "#datepicker" ).datepicker();
-  } );
-  
-  $( function() {
-    $( "#datepicker2" ).datepicker();
-  } );
+$(function () {
+    $("#datepicker").datepicker();
+});
 
-var dateToday = new Date(); 
+$(function () {
+    $("#datepicker2").datepicker();
+});
+
+var dateToday = new Date();
 
 $("#datepicker").datepicker({
     dateFormat: 'dd-mm-yy',
@@ -201,34 +199,34 @@ $("#datepicker2").datepicker({
 });
 
 
-$(document).on('resize, ready', function() {
+$(document).on('resize, ready', function () {
     // Add class if screen size equals
     var $window = $(window),
-    $navbar = $('#navbar-lg');
-   
-    function resize() {
-   
-       if ($window.width() < 768) {
-        scrollWindow();
-         return $navbar.attr('id', 'navbar');
-         /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-       } else if ($window.width() > 768) {
-        return $navbar.attr('id', 'navbar-lg');        
-       }
-     }
-     $window.resize(resize).trigger('resize');
-   });
-   function scrollWindow() {
-    var prevScrollpos = window.pageYOffset;
-    window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0px";
-        document.getElementById("navbar").style.transition = "0.3s ease";
-    } else {
-        document.getElementById("navbar").style.top = "-55px";
-    }
-    prevScrollpos = currentScrollPos;
-    }
-   }
+        $navbar = $('#navbar-lg');
 
+    function resize() {
+
+        if ($window.width() < 768) {
+            scrollWindow();
+            return $navbar.attr('id', 'navbar');
+            /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+        } else if ($window.width() > 768) {
+            return $navbar.attr('id', 'navbar-lg');
+        }
+    }
+    $window.resize(resize).trigger('resize');
+});
+
+function scrollWindow() {
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function () {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("navbar").style.top = "0px";
+            document.getElementById("navbar").style.transition = "0.3s ease";
+        } else {
+            document.getElementById("navbar").style.top = "-55px";
+        }
+        prevScrollpos = currentScrollPos;
+    }
+}
