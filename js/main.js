5e1e0842e32d0a2f7d3b53ae6b554db725210aca
Erind Hoxha <932400893@cit3-501m-12.acg.ac.nz>
2018-12-03 09:26:20 +1300

    fixed the stufz

--- js/main.js
+++ js/main.js
@@ -6,6 +6,11 @@ $(function () {
     }, 00);
 });
 
+var info = document.getElementById('summative-template').innerHTML;
+var template = Handlebars.compile(info);
+var dataTemplate = template(accommodation);
+var templateWrite = document.getElementById('card-container').innerHTML += dataTemplate;
+
 var transpOrAcc = "";
 var numberOfDays = "";
 var totalPrice = "";
@@ -186,6 +191,8 @@ $('.btn-search').on('click', function () {
     } else {
         $(".success-coupon").hide(300);
         $(".alert-coupon").show(300);
+        $("#card-coupon-code").css('border', '1px solid #ebebeb');   
+        $(".total-price").text('$' + totalPrice);              
     }
     });
 
@@ -797,26 +804,51 @@ document.querySelector('#card-number').addEventListener('keydown', (e) => {
     e.target.value = e.target.value.replace(/(\d{4})(\d+)/g, '$1 $2')
 })
 /* Jquery */
+$(".card-number-input").css('width', '100%');
+for (var i = 2; i < 5; i++) {
+    $(".card-change" + i).hide();
+}
+$(".card-change").hide();
+
 $('#card-number').keyup(function() {
+    // VISA CARD
     if ($("#card-number").val().startsWith("4")) {
         $(".visa-card").css('transform', 'scale(1.3)');
+        $(".card-number-input").css('width', '89%');
+        $(".card-change").show();
     }else {
+        $(".card-number-input").css('width', '100%');
+        $(".card-change").hide();
         $(".visa-card").css('transform', 'scale(1)');
     }
+
+    // MASTER CARD
     if ($("#card-number").val().startsWith("5")) {
         $(".master-card").css('transform', 'scale(1.3)');
+        $(".card-number-input").css('width', '89%');
+        $(".card-change2").show();
     }else {
         $(".master-card").css('transform', 'scale(1)');
+        $(".card-change2").hide();
     }
+
+    // DISCOVER CARD
     if ($("#card-number").val().startsWith("2")) {
         $(".discover-card").css('transform', 'scale(1.3)');
+        $(".card-number-input").css('width', '89%');
+        $(".card-change3").show();
     }else {
         $(".discover-card").css('transform', 'scale(1)');
+        $(".card-change3").hide();
     }
+    // AMERICAN EXPRESS CARD
     if ($("#card-number").val().startsWith("3")) {
         $(".american-express-card").css('transform', 'scale(1.3)');
+        $(".card-number-input").css('width', '89%');
+        $(".card-change4").show();
     } else {
         $(".american-express-card").css('transform', 'scale(1)');
+        $(".card-change4").hide();
     }
     
   $(this).val($(this).val().replace(/(\d{4})(\d+)/g, '$1 $2'))
@@ -830,3 +862,11 @@ $(".help-cvc").on('mouseout', function() {
     $(".help-cvc-div").hide(300);
 })
 
+var buttonPay = $(".btn-pay");
+
+buttonPay.on('click', function() {
+    if ($(".card-number-input").val() != "" && $("#card-expiration-date").val() != "" && $("#card-expiration-date-to").val() != "" && $("#card-cvc").val() != "") {
+        alertify.alert('Payment Successful!', 'Success! Your receipt will be sent into your email shortly.');
+        $(".ajs-close").css('float', 'right');
+    }
+});
