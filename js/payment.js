7a53c28a315b601c970b49d3e8fb260ab085a272
Erind Hoxha <932400893@cit3-501m-12.acg.ac.nz>
2018-12-03 11:25:05 +1300

    the credit card now shows on the right side with an animation

--- js/payment.js
+++ js/payment.js
@@ -16,7 +16,7 @@ $('#card-number').keyup(function() {
   if ($("#card-number").val().startsWith("4")) {
       $(".visa-card").css('transform', 'scale(1.3)');
       $(".card-number-input").css('width', '89%');
-      $(".card-change").show();
+      $(".card-change").show(200);
   }else {
       $(".card-number-input").css('width', '100%');
       $(".card-change").hide();
@@ -27,7 +27,7 @@ $('#card-number').keyup(function() {
   if ($("#card-number").val().startsWith("5")) {
       $(".master-card").css('transform', 'scale(1.3)');
       $(".card-number-input").css('width', '89%');
-      $(".card-change2").show();
+      $(".card-change2").show(200);
   }else {
       $(".master-card").css('transform', 'scale(1)');
       $(".card-change2").hide();
@@ -37,7 +37,7 @@ $('#card-number').keyup(function() {
   if ($("#card-number").val().startsWith("2")) {
       $(".discover-card").css('transform', 'scale(1.3)');
       $(".card-number-input").css('width', '89%');
-      $(".card-change3").show();
+      $(".card-change3").show(200);
   }else {
       $(".discover-card").css('transform', 'scale(1)');
       $(".card-change3").hide();
@@ -46,7 +46,7 @@ $('#card-number').keyup(function() {
   if ($("#card-number").val().startsWith("3")) {
       $(".american-express-card").css('transform', 'scale(1.3)');
       $(".card-number-input").css('width', '89%');
-      $(".card-change4").show();
+      $(".card-change4").show(200);
   } else {
       $(".american-express-card").css('transform', 'scale(1)');
       $(".card-change4").hide();
