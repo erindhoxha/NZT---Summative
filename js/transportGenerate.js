ff459ae11e073867ec81499c78394299580d6567
Erind Hoxha <932400893@cit3-501m-20.acg.ac.nz>
2018-12-05 11:27:58 +1300

    fixed

--- js/transportGenerate.js
+++ js/transportGenerate.js
@@ -1,31 +1,65 @@
 
 
 $(".btn-generate").on('click', function() { 
-  var petrol = 2.3;
   console.log('hi');
   var diesel = 1.65;
+  var petrol = 2.3;
+  var fuelPriceTotal;
+
   var selectOption = $( ".fuel-select-dropdown option:selected" ).val();
+  var selectOptionFuel = $( ".fuel-select-dropdown-fuel option:selected" ).val();
   var inputValue = $("#fuel-input").val();
   inputValue = parseInt(inputValue);
   console.log(inputValue);
+
+  if (selectOptionFuel == "Petrol") {
+    fuelPriceTotal = petrol;
+  } else {
+    fuelPriceTotal = diesel;
+  }
+  if ($("#fuel-input").val().length > 12) {
+    $(".liter-price").text("Whoops, that's too many KM's.");
+    return;
+  }
+  if ($("#fuel-input").val() != "") {
+
   if (selectOption == "Motorbike") {
     var val = Math.round(inputValue * 0.037 * 100) / 100;
     console.log(val);
     $('.fuel-liter').text("You will use "); 
     $(".fuel-total").text(val + "L");
     $(".liter-price").text("Will approximately cost: ");
-    $(".fuel-total-price").text(val);
+    var total = val * fuelPriceTotal;
+    total = Math.round(total);
+    $(".fuel-total-price").text("$" + total); 
   }
   if (selectOption == "Small Car") {
     var val = Math.round(inputValue * 0.085 * 100) / 100;
-    $('.fuel-liter').text("You will use " + val + "L which will approximately cost you: " + literPrice); 
+    $('.fuel-liter').text("You will use "); 
+    $(".fuel-total").text(val + "L");
+    $(".liter-price").text("Will approximately cost: ");
+    var total = val * fuelPriceTotal;
+    total = Math.round(total);
+    $(".fuel-total-price").text("$" + total); 
   }
   if (selectOption == "Large Car") {
     var val = Math.round(inputValue * 0.097 * 100) / 100;
-    $('.fuel-liter').text("You will use " + val + "L which will approximately cost you: " + literPrice); 
+    $('.fuel-liter').text("You will use "); 
+    $(".fuel-total").text(val + "L");
+    $(".liter-price").text("Will approximately cost: ");
+    var total = val * fuelPriceTotal;
+    total = Math.round(total);
+    $(".fuel-total-price").text("$" + total); 
   }
   if (selectOption == "Motor Van") {
     var val = Math.round(inputValue * 0.17 * 100) / 100;
-    $('.fuel-liter').text("You will use " + val + "L which will approximately cost you: "  + literPrice); 
+    $('.fuel-liter').text("You will use "); 
+    $(".fuel-total").text(val + "L");
+    $(".liter-price").text("Will approximately cost: ");
+    var total = val * fuelPriceTotal;
+    total = Math.round(total);
+    $(".fuel-total-price").text("$" + total); 
   }
+}
+
 });
